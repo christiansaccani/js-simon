29640268d5758404f54d8caf8754111f681a981f
christiansaccani <christian.saccani99@gmail.com>
2024-02-09 15:35:15 +0100

    timer is working

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // getTime() mi ristituisce il tempo in ms che intercorre sino alla data da me indicata
-var countDownDay = new Date("Jan 5, 2024 15:37:25").getTime();
-
-// Trovo il corrispettivo del valore precedente del momento in cui avvio la pagina
-var now = new Date().getTime();
-
-// La differenza tra l'ora da raggiungere e quella attuale in ms
-var timeDiff = countDownDay - now;
+var countDownDay = new Date("Feb 12, 2024 09:30:00").getTime();
 
 // questo rieseguirà la mia funzione ogni secondo;
-// setInterval (counterTimer, 1000);
+setInterval (counterTimer, 1000);
 
 function counterTimer () {
+
+    // Trovo il corrispettivo del valore precedente del momento in cui avvio la pagina
+    var now = new Date().getTime();
+
+    // La differenza tra l'ora da raggiungere e quella attuale in ms
+    var timeDiff = countDownDay - now;
 
     // calcolo in unità di misura partendo dai ms
     // il primo calcolo considera quanti, ad es secondi, sono necessari a fare un minuto;
@@ -20,5 +20,7 @@ function counterTimer () {
     var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
-    console.log(seconds, minutes, hours, days);
+    document.querySelector("div").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
 }
+
